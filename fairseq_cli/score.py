7fdc60c57f49cae7ad90150739ee179701dfc468
Fei-WL <573943538@qq.com>
2021-01-15 01:05:07 +0800

    在fairseq_cli/score.py中增加显式输出

--- fairseq_cli/score.py
+++ fairseq_cli/score.py
@@ -55,10 +55,10 @@ def cli_main():
 
     if args.sacrebleu:
         import sacrebleu
-
+        print("sacrebleu")
         def score(fdsys):
             with open(args.ref) as fdref:
-                print(sacrebleu.corpus_bleu(fdsys, [fdref]))
+                print(sacrebleu.corpus_bleu(fdsys, [fdref], force=True))
 
     elif args.sentence_bleu:
 
